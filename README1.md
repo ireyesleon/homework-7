c3906ba111f1c6d8f058320a5f8418f9f08f9588
Isaías Reyes León <elreyieon@Isaiass-MacBook-Air.local>
2022-01-17 16:27:53 -0600

    Added logic to created readme file, pending some fixes in the file name.

---
+++ README1.md
@@ -0,0 +1,26 @@
+# test
+  * [Description](#descritpion)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#test)
+  * [License](#license)
+  * [Questions](#questions)
+  
+  ## Descritpion
+  test
+  ## Installation
+  test
+  ## Usage
+  test
+  ## Contribution
+  test
+  ## Tests
+  test
+  ## License
+  ISC
+  ## Questions
+  Please feel free to reach
+  GitHub: https://github.com/ireyesleon
+  Email: izaaiaz@gmail.com
+  
--- index.js
+++ index.js
@@ -41,19 +41,56 @@ inquirer
       name: 'license',
       choices: ['ISC', 'Apache', 'MIT', 'GNU General v2', 'GNU General v3', 'GNU Affero', 'GNU Lesser General v2.1', 'BSD-2', 'BSD-3', 'Boost Software', 'Creative Commons Zero', 'Eclipse', ''],
     },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please enter your GitHub user:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your email address:',
+    }
   ])
 
 // TODO: Create a function to write README file
 .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    fs.writeFile(`README1.md`, generateMarkdown(data), (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
 
+function generateMarkdown(data) {
+    return `# ${data.title}
+  * [Description](#descritpion)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#test)
+  * [License](#license)
+  * [Questions](#questions)
+  
+  ## Descritpion
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contribution
+  ${data.contribution}
+  ## Tests
+  ${data.test}
+  ## License
+  ${data.license}
+  ## Questions
+  Please feel free to reach
+  GitHub: https://github.com/${data.github}
+  Email: ${data.email}
+  `;
+  }
+
 // TODO: Create a function to initialize app
-// function init() {}
+function init() {}
 
 // Function call to initialize app
-// init();
+init();
